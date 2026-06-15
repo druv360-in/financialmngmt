e0aae23d078f263dc600209b2fa2d8e2ef11fc7a
Vimal Krishna T <vimaltvarier@gmail.com>
2026-06-15 20:06:47 +0530

    Vimal Component updated , page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
-// arathi
-import Addoreditbill from './components/arathi/Addoreditbill'
-import Receiptcard from './components/arathi/Receiptcard'
-import Receiptsmanagement from './components/arathi/Receiptsmanagement'
-
-// binoj
-import CurrentBalances from './components/binoj/CurrentBalances'
-import FinancialYearBanner from './components/binoj/FinancialYearBanner'
-import Sidemenu from './components/binoj/Sidemenu'
-import TotalDisplayCards from './components/binoj/TotalDisplayCards'
+// vimal
+import BillsManagementPage from "./pages/BillsManagementPage"
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/Addoreditbill"      element={<Addoreditbill />} />
-      <Route path="/Receiptcard"        element={<Receiptcard />} />
-      <Route path="/Receiptsmanagement" element={<Receiptsmanagement />} />
-      <Route path="/CurrentBalances"    element={<CurrentBalances />} />
-      <Route path="/FinancialYearBanner" element={<FinancialYearBanner />} />
-      <Route path="/Sidemenu"            element={<Sidemenu />} />
-      <Route path="/TotalDisplayCards"        element={<TotalDisplayCards />} />
+      <Route path="/BillsPage" element={<BillsManagementPage/>}/>
     </Routes>
   )
 }
