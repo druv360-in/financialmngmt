f3e818d537cf219859293797392ecc9eda8cea12
shillaCV <shillaraveendran2003@gmail.com>
2026-06-15 20:35:15 +0530

    shilla updated components with pages (#23)
    Co-authored-by: Shilla <your-email@gmail.com>
    Co-authored-by: druv360-in <druv360@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 import { Routes, Route } from 'react-router-dom'
 
 import Page5 from './pages/Page5'
@@ -5,16 +7,18 @@ import Page5 from './pages/Page5'
 import BillsManagementPage from "./pages/BillsManagementPage"
 import DashboardPage from "./pages/DashboardPage";
 
+import CategoryPage from './pages/categoryPage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/CategoryPage" element={<CategoryPage />} />
       <Route path="/receipts"               element={<Page5 />}  />
      
       <Route path="/BillsPage" element={<BillsManagementPage/>}/>
       <Route path="/" element={<DashboardPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
