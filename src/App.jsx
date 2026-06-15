1ac7ad7d8f2bcb9b71db182afb2871488dbf8ac1
VIMAL KRISHNA T <144359219+VimalKrishnaT@users.noreply.github.com>
2026-06-15 20:16:09 +0530

    Vimal Component updated , page added (#21)
    Co-authored-by: druv360-in <druv360@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
+// vimal
+import BillsManagementPage from "./pages/BillsManagementPage"
 import DashboardPage from "./pages/DashboardPage";
 
 
 function App() {
   return (
     <Routes>
+      <Route path="/BillsPage" element={<BillsManagementPage/>}/>
       <Route path="/" element={<DashboardPage />} />
     </Routes>
   )
