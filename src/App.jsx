fbb822d17cf280aa25c46146e014f3cf6b28ec8b
FathimaRaheeba <fathimaraheeba8@gmail.com>
2026-06-16 15:51:44 +0530

    page added with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
 
-// arathi
-import Addoreditbill from './components/arathi/Addoreditbill'
-import Receiptcard from './components/arathi/Receiptcard'
-import Receiptsmanagement from './components/arathi/Receiptsmanagement'
-
-// binoj
-import CurrentBalances from './components/binoj/CurrentBalances'
-import FinancialYearBanner from './components/binoj/FinancialYearBanner'
-import Sidemenu from './components/binoj/Sidemenu'
-import TotalDisplayCards from './components/binoj/TotalDisplayCards'
+import BulkDues from './pages/BulkDues';
+import CreateBulkDue from "./components/fathima/CreateBulkDue";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/Addoreditbill"      element={<Addoreditbill />} />
-      <Route path="/Receiptcard"        element={<Receiptcard />} />
-      <Route path="/Receiptsmanagement" element={<Receiptsmanagement />} />
-      <Route path="/CurrentBalances"    element={<CurrentBalances />} />
-      <Route path="/FinancialYearBanner" element={<FinancialYearBanner />} />
-      <Route path="/Sidemenu"            element={<Sidemenu />} />
-      <Route path="/TotalDisplayCards"        element={<TotalDisplayCards />} />
+      <Route path="/" element={<BulkDues />} />
+        <Route path="/bulk-dues" element={<BulkDues />} />
+       <Route path="/create-bulk-due" element={<CreateBulkDue />} />
     </Routes>
   )
 }
