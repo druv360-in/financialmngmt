3684e228ba6801318cd493c9983aea57324e88b9
https://github.com/mpsreekaanth-code/Sreek-portfolio <mpsreekaanth@gmail.com>
2026-06-16 16:07:05 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 
-// arathi
-import Addoreditbill from './components/arathi/Addoreditbill'
-import Receiptcard from './components/arathi/Receiptcard'
-import Receiptsmanagement from './components/arathi/Receiptsmanagement'
-
-// binoj
-import CurrentBalances from './components/binoj/CurrentBalances'
-import FinancialYearBanner from './components/binoj/FinancialYearBanner'
-import Sidemenu from './components/binoj/Sidemenu'
-import TotalDisplayCards from './components/binoj/TotalDisplayCards'
 
 // sreekaanth
-import CategoryWiseReportPage from './pages/Sreekaanth/CategoryWiseReportPage'
-import DebitCategoryWiseReportPage from './pages/Sreekaanth/DebitCategoryWiseReportPage'
-import DuesReportPage from './pages/Sreekaanth/DuesReportPage'
-import FinancialReportsPage from './pages/Sreekaanth/FinancialReportsPage'
+import CategoryWiseReportPage from './pages/CategoryWiseReportPage'
+import DebitCategoryWiseReportPage from './pages/DebitCategoryWiseReportPage'
+import DuesReportPage from './pages/DuesReportPage'
+import FinancialReportsPage from './pages/FinancialReportsPage'
 
 
 function App() {
   return (
     <Routes>
 
-      {/* arathi */}
-      <Route path="/Addoreditbill"      element={<Addoreditbill />} />
-      <Route path="/Receiptcard"        element={<Receiptcard />} />
-      <Route path="/Receiptsmanagement" element={<Receiptsmanagement />} />
-
-      {/* binoj */}
-      <Route path="/CurrentBalances"     element={<CurrentBalances />} />
-      <Route path="/FinancialYearBanner" element={<FinancialYearBanner />} />
-      <Route path="/Sidemenu"            element={<Sidemenu />} />
-      <Route path="/TotalDisplayCards"   element={<TotalDisplayCards />} />
 
       {/* sreekaanth */}
       <Route path="/financial-reports"          element={<FinancialReportsPage />} />
