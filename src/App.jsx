3c5001103dc7e0b7afd4de523f7a2193eb2707cc
anfas <anfas@MacBook-Air.local>
2026-06-18 19:52:12 +0530

    anfas updated pages and compnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import JournalPage from "./pages/JournalPage";
 
-// arathi
-import Addoreditbill from './components/arathi/Addoreditbill'
-import Receiptcard from './components/arathi/Receiptcard'
-import Receiptsmanagement from './components/arathi/Receiptsmanagement'
-
-// binoj
-import CurrentBalances from './components/binoj/CurrentBalances'
-import FinancialYearBanner from './components/binoj/FinancialYearBanner'
-import Sidemenu from './components/binoj/Sidemenu'
-import TotalDisplayCards from './components/binoj/TotalDisplayCards'
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/Addoreditbill"      element={<Addoreditbill />} />
-      <Route path="/Receiptcard"        element={<Receiptcard />} />
-      <Route path="/Receiptsmanagement" element={<Receiptsmanagement />} />
-      <Route path="/CurrentBalances"    element={<CurrentBalances />} />
-      <Route path="/FinancialYearBanner" element={<FinancialYearBanner />} />
-      <Route path="/Sidemenu"            element={<Sidemenu />} />
-      <Route path="/TotalDisplayCards"        element={<TotalDisplayCards />} />
+      <Route path="/ledger"    element={<JournalPage />} />
+      
     </Routes>
   )
 }
